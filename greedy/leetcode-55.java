3f3d79521988ce1bf0306c7d6edfb2c5d86b0205
cainiao-help <784498158@qq.com>
2025-09-09 14:56:11 +0800

    Update leetcode-55.java

--- greedy/leetcode-55.java
+++ greedy/leetcode-55.java
@@ -1,1 +1,41 @@
+/*
 
+55. 跳跃游戏
+给你一个非负整数数组 nums ，你最初位于数组的 第一个下标 。数组中的每个元素代表你在该位置可以跳跃的最大长度。
+判断你是否能够到达最后一个下标，如果可以，返回 true ；否则，返回 false 。
+
+示例 1：
+输入：nums = [2,3,1,1,4]
+输出：true
+解释：可以先跳 1 步，从下标 0 到达下标 1, 然后再从下标 1 跳 3 步到达最后一个下标。
+
+示例 2：
+输入：nums = [3,2,1,0,4]
+输出：false
+解释：无论怎样，总会到达下标为 3 的位置。但该下标的最大跳跃长度是 0 ， 所以永远不可能到达最后一个下标。
+
+提示：
+1 <= nums.length <= 104
+0 <= nums[i] <= 105
+
+*/
+
+// 贪心算法，维护最右可以到达的区间。比较当前位置能跳到的最大值，若大于最大值（最右值）则修改最大值，若小于则继续遍历。如果遍历的数大于最大值则无法到达。
+
+    public static void main(String[] args) {
+        int[] nums = {2,3,1,1,4};
+        System.out.println(canJump(nums));
+
+
+    }
+
+    public static  boolean canJump(int[] nums) {
+        int mx = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > mx) { // 无法到达 i
+                return false;
+            }
+            mx = Math.max(mx, i + nums[i]); // 从 i 最右可以跳到 i + nums[i]
+        }
+        return true;
+    }

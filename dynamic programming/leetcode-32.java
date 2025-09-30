ac4136a8d53753b3fa5b478d60566080b382c02c
cainiao-help <784498158@qq.com>
2025-09-30 17:08:48 +0800

    Create leetcode-32.java

---
+++ dynamic programming/leetcode-32.java
@@ -0,0 +1,45 @@
+/*
+32. 最长有效括号
+给你一个只包含 '(' 和 ')' 的字符串，找出最长有效（格式正确且连续）括号 子串 的长度。
+左右括号匹配，即每个左括号都有对应的右括号将其闭合的字符串是格式正确的，比如 "(()())"。
+
+示例 1：
+输入：s = "(()"
+输出：2
+解释：最长有效括号子串是 "()"
+  
+示例 2：
+输入：s = ")()())"
+输出：4
+解释：最长有效括号子串是 "()()"
+  
+示例 3：
+输入：s = ""
+输出：0
+
+提示：
+0 <= s.length <= 3 * 104
+s[i] 为 '(' 或 ')'
+*/
+
+// 可以使用动态规划记录当前最大有效括号，
+
+    public static void main(String[] args) {
+        String s = "()(())";
+        System.out.println(longestValidParentheses(s));
+    }
+    public static int longestValidParentheses(String s) {
+        int maxans = 0;
+        int[] dp = new int[s.length()];
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                if (s.charAt(i - 1) == '(') {
+                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                    dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+                }
+                maxans = Math.max(maxans, dp[i]);
+            }
+        }
+        return maxans;
+    }

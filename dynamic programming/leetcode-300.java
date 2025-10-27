e812a6453edd40b956fadeeb947d4173db7156ca
cainiao-help <784498158@qq.com>
2025-10-27 15:39:46 +0800

    Update leetcode-300.java

--- dynamic programming/leetcode-300.java
+++ dynamic programming/leetcode-300.java
@@ -49,3 +49,39 @@
         }
         return res;
     }
+
+
+// 以下为贪心+二分方法，时间复杂度优化为O（nlogn）
+
+    public static void main(String[] args) {
+        int[] nums = {1,3,6,7,9,4,10,2,3,4,5,6,7};
+        System.out.println(lengthOfLIS(nums));
+    }
+
+
+    public static int lengthOfLIS(int[] nums){
+        int len = 1, n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int[] d = new int[n + 1];
+        d[len] = nums[0];
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] > d[len]) {
+                d[++len] = nums[i];
+            } else {
+                int l = 1, r = len, pos = 0; // 如果找不到说明所有的数都比 nums[i] 大，此时要更新 d[1]，所以这里将 pos 设为 0
+                while (l <= r) {
+                    int mid = (l + r) >> 1;
+                    if (d[mid] < nums[i]) {
+                        pos = mid;
+                        l = mid + 1;
+                    } else {
+                        r = mid - 1;
+                    }
+                }
+                d[pos + 1] = nums[i];
+            }
+        }
+        return len;
+    }
